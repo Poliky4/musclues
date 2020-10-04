dcda23b3116373144b1d9809fc168dde5771dffe
Poliky4 <pontusmolin@me.com>
2020-10-04 15:59:39 +0200

    more animations + fixes

--- js/script.js
+++ js/script.js
@@ -35,6 +35,13 @@ const totalHeight = sum([
   head_height
 ])
 
+const origin_height = (
+  foot_height
+  + lower_leg_height
+  + upper_leg_height
+  + padding * 3
+)
+
 // <Init>
 const mats = {}
 const scene = createScene()
@@ -50,7 +57,7 @@ window.addEventListener("resize", () => {
 
 function createScene() {
   const scene = new b.Scene(engine)
-  // scene.debugLayer.show()
+  scene.debugLayer.show()
 
   mats.transparent = makeTransparentMaterial(scene)
   mats.origin = makeOriginMaterial(scene)
@@ -67,24 +74,50 @@ function createScene() {
       0, totalHeight/2, -40))
   camera.attachControl(canvas, true)
 
-  const light = new b.HemisphericLight
+  const light_hemispheric = new b.HemisphericLight
   (
-    "light1",
+    "light_hemispheric",
     new b.Vector3(0, 1, 0),
     scene
   )
+  light_hemispheric.intensity = 0.2
+  var light_spot = new BABYLON.SpotLight(
+    "light_spot",
+    new BABYLON.Vector3(20, 40, -20),
+    new BABYLON.Vector3(-4, -5, 4),
+    Math.PI / 3, 2,
+    scene
+  );
+  const shadow_generator_light_spot = new b.ShadowGenerator(1024, light_spot);
+  const light_spot_box = b.MeshBuilder.CreateBox(
+    "light_spot_box",
+    {
+      height: 0.1,
+      width: 0.1,
+      depth: 0.1,
+    },
+    scene
+  )
+  light_spot_box.position = light_spot.position
 
-  const dude = makeDude(scene)
+  const { dude } = makeDude(scene)
   camera.lockTarget = dude
+  shadow_generator_light_spot.addShadowCaster(dude)
+  console.log('dude: ', dude);
 
   return scene
 }
+const ground = BABYLON.MeshBuilder.CreateGround("ground", {
+  width: 150,
+  height: 150,
+}, scene);
+ground.receiveShadows = true;
 
 function makeTransparentMaterial(scene) {
   const mat =
     new b.StandardMaterial(
       "mat_transparent", scene)
-  mat.alpha = 0.5
+  // mat.alpha = 0.5
   
   return mat
 }
@@ -110,12 +143,7 @@ function makeDude(scene) {
     scene
   )
   origin.material = mats.origin
-  origin.position.y = (
-    foot_height
-    + lower_leg_height
-    + upper_leg_height
-    + padding * 3
-  )
+  origin.position.y = origin_height
 
   const torso = b.MeshBuilder.CreateBox(
     "torso",
@@ -232,10 +260,17 @@ function makeDude(scene) {
   ) 
   foot_l.material = mats.transparent
   foot_l.parent = lower_leg_l
-  foot_l.position.z = -1.5
   foot_l.position.y = -(
     lower_leg_height / 2
     + 1
+  )
+  foot_l.setPivotMatrix(
+    b.Matrix.Translation(
+      0,
+      0,
+      -((5 / 2) - 1), // -1 = half leg thickness
+    ),
+    false
   )
 
   const upper_leg_r = b.MeshBuilder.CreateBox(
@@ -301,10 +336,17 @@ function makeDude(scene) {
   ) 
   foot_r.material = mats.transparent
   foot_r.parent = lower_leg_r
-  foot_r.position.z = -1.5
   foot_r.position.y = -(
     lower_leg_height / 2
     + 1
+  )
+  foot_r.setPivotMatrix(
+    b.Matrix.Translation(
+      0,
+      0,
+      -((5 / 2) - 1), // -1 = half leg thickness
+    ),
+    false
   )
 
   const upper_arm_l = b.MeshBuilder.CreateBox(
@@ -458,13 +500,16 @@ function makeDude(scene) {
     elbow_r: lower_arm_r,
     hip_r: upper_leg_r,
     knee_r: lower_leg_r,
+    foot_r,
     hip_l: upper_leg_l,
     knee_l: lower_leg_l,
+    foot_l,
   }
 
   const rotateMesh = (meshName, rotation) => {
     if (!meshMap[meshName]) return console.warn("Unknown mesh:", meshName)
     rotate(meshMap[meshName], rotation)
+    return meshMap[meshName].rotation
   }
   window.rotateMesh = rotateMesh
   const setRotationMesh = (meshName, rotation) => {
@@ -474,7 +519,7 @@ function makeDude(scene) {
   window.setRotationMesh = setRotationMesh
 
   const FPS = 30
-  const animationDurationMs = 300
+  const animationDurationMs = 600
   const frames = (FPS/1000) * animationDurationMs
   const frameDuration = animationDurationMs/frames
   const playAnimation = (to, from) => {
@@ -485,7 +530,16 @@ function makeDude(scene) {
   }
   function applyPose(pose) {
     Object.entries(pose).forEach(([name, rotation]) => {
-      setRotationMesh(name, rotation)
+      switch (name) {
+        case "height": {
+          origin.position.y = rotation.y
+          break;
+        }
+        default: {
+          setRotationMesh(name, rotation)
+          break;
+        }
+      }
     })
   }
   function playSteps(steps, frames, frameDuration) {
@@ -518,6 +572,7 @@ function makeDude(scene) {
 
   function getCurrentPose() {
     return {
+      height: { y:origin.position.y },
       torso: getMeshRotation(torso),
       shoulder_l: getMeshRotation(upper_arm_l),
       elbow_l: getMeshRotation(lower_arm_l),
@@ -525,8 +580,10 @@ function makeDude(scene) {
       elbow_r: getMeshRotation(lower_arm_r),
       hip_r: getMeshRotation(upper_leg_r),
       knee_r: getMeshRotation(lower_leg_r),
+      foot_r: getMeshRotation(foot_r),
       hip_l: getMeshRotation(upper_leg_l),
-      knee_l: getMeshRotation(lower_leg_l)
+      knee_l: getMeshRotation(lower_leg_l),
+      foot_l: getMeshRotation(foot_l),
     }
   }
   function getMeshRotation(mesh) {
@@ -535,19 +592,23 @@ function makeDude(scene) {
   }
 
   const poses = makePoses()
-  repeatReverse(poses.zero, poses.rest)
+  applyPose(poses.rest)
+  repeatReverse(poses.wave_end, poses.wave_start)
+  // applyPose(poses.squat_end)
+  // repeatReverse(poses.squat_end, poses.squat_start)
+  // repeatReverse(poses.pushup_start, poses.pushup_end)
 
-  function repeatReverse(start, end) {
+  function repeatReverse(start, end, duration = animationDurationMs) {
     let flip = true
     setInterval(() => {
       if(flip) playAnimation(start)
       else playAnimation(end)
       flip = !flip
-    }, 1000)
+    }, duration)
   }
 
   return {
-    dude: torso,
+    dude: origin,
     applyPose,
     playAnimation
   }
@@ -563,28 +624,42 @@ function rotate(mesh, rotation) {
   mesh.rotation.y += rotation.y ?? 0
   mesh.rotation.z += rotation.z ?? 0
 }
+function setPosition(mesh, position) {
+  mesh.position.x = position.x ?? mesh.position.x
+  mesh.position.y = position.y ?? mesh.position.y
+  mesh.position.z = position.z ?? mesh.position.z
+}
+function position(mesh, position) {
+  mesh.position.x += position.x ?? 0
+  mesh.position.y += position.y ?? 0
+  mesh.position.z += position.z ?? 0
+}
 
 /**
+ * height: { y: y pos for origin }
  * torso { x: forward/back, y: rot, z: left/right }
  * shoulder { x: rot, y: forward/back , z: up/down}
  * elbow { y }
  * hip { x: forward/back, y: rot, z: left/right }
  * knee { x }
+ * foot { x }
  */
 function makePoses() {
   return {
     zero: {
+      height:       { y: origin_height },
       torso:        { x: 0, y: 0, z: 0 },
       shoulder_l:   { x: 0, y: 0, z: 0 },
       elbow_l:      { y: 0 },
       shoulder_r:   { x: 0, y: 0, z: 0 },
       elbow_r:      { y: 0 },
-      hip_r:        { x: 0, y: 0, z: 0 },
-      knee_r:       { x: 0 },
       hip_l:        { x: 0, y: 0, z: 0 },
       knee_l:       { x: 0 },
+      hip_r:        { x: 0, y: 0, z: 0 },
+      knee_r:       { x: 0 },
     },
     rest: {
+      height:       { y: origin_height },
       shoulder_l:   { x: -0.2, y: 0.4, z: -1.55 },
       elbow_l:      { y: 0.4 },
       shoulder_r:   { x: -0.2, y: -0.4, z: 1.55 },
@@ -593,6 +668,80 @@ function makePoses() {
       knee_l:       { x: -0.1 },
       hip_r:        { x: 0.1, y: 0.2 },
       knee_r:       { x: -0.1 },
-    }
+    },
+    wave_start: {
+      height:       { y: origin_height },
+      shoulder_l:   { x: 2.4, y: -1.4, z: -1.55 },
+      elbow_l:      { y: 0 },
+      shoulder_r:   { x: -0.2, y: -0.4, z: 1.55 },
+      elbow_r:      { y: -0.4 },
+      hip_l:        { x: 0.1, y: -0.2 },
+      knee_l:       { x: -0.1 },
+      hip_r:        { x: 0.1, y: 0.2 },
+      knee_r:       { x: -0.1 },
+    },
+    wave_end: {
+      height:       { y: origin_height },
+      shoulder_l:   { x: 3, y: -1.4, z: -1.55 },
+      elbow_l:      { y: 1 },
+      shoulder_r:   { x: -0.2, y: -0.4, z: 1.55 },
+      elbow_r:      { y: -0.4 },
+      hip_l:        { x: 0.1, y: -0.2 },
+      knee_l:       { x: -0.1 },
+      hip_r:        { x: 0.1, y: 0.2 },
+      knee_r:       { x: -0.1 },
+    },
+    pushup_start: {
+      height:       { y: origin_height },
+      torso:        { x: -1.55 },
+      shoulder_l:   { x: -1.2, y: 0.2, z: -1.6 },
+      elbow_l:      { y: 2.7 },
+      shoulder_r:   { x: -1.2, y: -0.2, z: 1.6 },
+      elbow_r:      { y: -2.7 },
+      hip_l:        { x: -1.55 },
+      knee_l:       { x: 0 },
+      hip_r:        { x: -1.55 },
+      knee_r:       { x: 0 },
+    },
+    pushup_end: {
+      height:       { y: origin_height },
+      torso:        { x: -1.55 },
+      shoulder_l:   { x: 1.6, y: 0, z: -1.6 },
+      elbow_l:      { y: 0 },
+      shoulder_r:   { x: 1.6, y: 0, z: 1.6 },
+      elbow_r:      { y: 0 },
+      hip_l:        { x: -1.55 },
+      knee_l:       { x: 0 },
+      hip_r:        { x: -1.55 },
+      knee_r:       { x: 0 },
+    },
+    squat_start: {
+      height:       { y: origin_height },
+      torso:        { x: 0, y: 0, z: 0 },
+      shoulder_l:   { x: -0.4, y: 0.6, z: -1.4 },
+      elbow_l:      { y: 1.3 },
+      shoulder_r:   { x: -0.4, y: -0.6, z: 1.4 },
+      elbow_r:      { y: -1.3 },
+      hip_l:        { x: 0.1, y: -0.2 },
+      knee_l:       { x: -0.1 },
+      foot_l:       { x: 0 },
+      hip_r:        { x: 0.1, y: 0.2 },
+      knee_r:       { x: -0.1 },
+      foot_r:       { x: 0 },
+    },
+    squat_end: {
+      height:       { y: 5 },
+      torso:        { x: -0.1, y: 0, z: 0 },
+      shoulder_l:   { x: -0.4, y: 0.6, z: -1.4 },
+      elbow_l:      { y: 1.3 },
+      shoulder_r:   { x: -0.4, y: -0.6, z: 1.4 },
+      elbow_r:      { y: -1.3 },
+      hip_l:        { x: 1.7, y: -0.2 },
+      knee_l:       { x: -2.5 },
+      foot_l:       { x: 0.8 },
+      hip_r:        { x: 1.7, y: 0.2 },
+      knee_r:       { x: -2.5 },
+      foot_r:       { x: 0.8 },
+    },
   }
 }
