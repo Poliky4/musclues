e8b20f549a3fa82461ee60c045c84e082f1c4613
Poliky4 <pontusmolin@me.com>
2021-02-06 11:58:33 +0100

    add typescript

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,4 +1,5 @@
 import * as b from "babylonjs";
+import { rotate, setRotation } from "./utils";
 
 const canvas = document.getElementsByTagName("canvas")[0];
 
@@ -504,30 +505,6 @@ function makeDude(scene) {
     applyPose,
     playAnimation,
   };
-}
-
-export function setRotation(mesh, rotation) {
-  mesh.rotation.x = rotation.x ?? mesh.rotation.x;
-  mesh.rotation.y = rotation.y ?? mesh.rotation.y;
-  mesh.rotation.z = rotation.z ?? mesh.rotation.z;
-}
-
-export function rotate(mesh, rotation) {
-  mesh.rotation.x += rotation.x ?? 0;
-  mesh.rotation.y += rotation.y ?? 0;
-  mesh.rotation.z += rotation.z ?? 0;
-}
-
-export function setPosition(mesh, position) {
-  mesh.position.x = position.x ?? mesh.position.x;
-  mesh.position.y = position.y ?? mesh.position.y;
-  mesh.position.z = position.z ?? mesh.position.z;
-}
-
-export function position(mesh, position) {
-  mesh.position.x += position.x ?? 0;
-  mesh.position.y += position.y ?? 0;
-  mesh.position.z += position.z ?? 0;
 }
 
 /**
---
+++ src/js/utils.ts
@@ -0,0 +1,23 @@
+export function setRotation(mesh, rotation) {
+  mesh.rotation.x = rotation.x ?? mesh.rotation.x;
+  mesh.rotation.y = rotation.y ?? mesh.rotation.y;
+  mesh.rotation.z = rotation.z ?? mesh.rotation.z;
+}
+
+export function rotate(mesh, rotation) {
+  mesh.rotation.x += rotation.x ?? 0;
+  mesh.rotation.y += rotation.y ?? 0;
+  mesh.rotation.z += rotation.z ?? 0;
+}
+
+export function setPosition(mesh, position) {
+  mesh.position.x = position.x ?? mesh.position.x;
+  mesh.position.y = position.y ?? mesh.position.y;
+  mesh.position.z = position.z ?? mesh.position.z;
+}
+
+export function position(mesh, position) {
+  mesh.position.x += position.x ?? 0;
+  mesh.position.y += position.y ?? 0;
+  mesh.position.z += position.z ?? 0;
+}
