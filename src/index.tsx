e4d48e3a1fb7ff0076adb618c7dba4c2748a8840
Poliky4 <pontusmolin@me.com>
2021-02-07 20:06:45 +0100

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,4 @@
 import { render } from "preact";
-import styled from "styled-components";
-import { useEffect } from "preact/hooks";
-import { musclues } from "./engine";
-import { Exercises } from "./components/Exercises";
-import { GlobalStyle } from "./globalStyle";
-import { Canvas } from "./components/Canvas";
-
-const StyledApp = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`;
-
-const App = () => {
-  useEffect(() => {
-    musclues();
-  }, []);
-
-  return (
-    <StyledApp>
-      <GlobalStyle />
-      <Canvas />
-      <Exercises />
-    </StyledApp>
-  );
-};
+import { App } from "./app";
 
 render(<App />, document.body);
