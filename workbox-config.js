d613b27718e4a6a79dfe37d02a136a485ee1dfd6
Poliky4 <pontusmolin@me.com>
2021-02-11 22:45:41 +0100

    add webmanifest to sw cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: "dist/",
-  globPatterns: ["**/*.{png,css,js,html}"],
+  globPatterns: ["**/*.{png,css,js,html,webmanifest}"],
   swDest: "dist/sw.js",
   maximumFileSizeToCacheInBytes: 5000000,
 };
